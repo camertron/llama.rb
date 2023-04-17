0f3e3ebd55808472aa7c8e4ad395d3b24733da4d
Cameron Dutro <camertron@gmail.com>
2023-04-16 21:47:12 -0700

    Convert indentation to spaces

--- ext/llama_rb/extconf.rb
+++ ext/llama_rb/extconf.rb
@@ -30,22 +30,22 @@ end
 # OS specific
 # TODO: support Windows
 if %w[Linux Darwin FreeBSD NetBSD OpenBSD Haiku].include?(uname_s)
-	$CFLAGS   << " -pthread"
-	$CXXFLAGS << " -pthread"
+  $CFLAGS   << " -pthread"
+  $CXXFLAGS << " -pthread"
 end
 
 # Architecture specific
 # TODO: these flags probably need to be tweaked on some architectures
 if uname_m.include?("x86_64") || uname_m.include?("i686")
   if uname_s == "Darwin"
-		cpu_features = `sysctl machdep.cpu.features`.strip
+    cpu_features = `sysctl machdep.cpu.features`.strip
     cpu_leaf7_features = `sysctl machdep.cpu.leaf7_features`.strip
 
     $CFLAGS << " -mf16c" if cpu_features.include?("F16C")
     $CFLAGS << " -mfma"  if cpu_features.include?("FMA")
     $CFLAGS << " -mavx"  if cpu_features.include?("AVX1.0")
     $CFLAGS << " -mavx2" if cpu_leaf7_features.include?("AVX2")
-	elsif uname_s == "Linux"
+  elsif uname_s == "Linux"
     cpuinfo = File.read("/proc/cpuinfo")
 
     $CFLAGS << " -mavx"        if cpuinfo =~ /\bavx\b/
@@ -68,9 +68,9 @@ if uname_m.include?("x86_64") || uname_m.include?("i686")
     $CFLAGS << " -mavx2" if sysinfo_cpu =~ /\bAVX2\b/
     $CFLAGS << " -mfma"  if sysinfo_cpu =~ /\bFMA\b/
     $CFLAGS << " -mf16c" if sysinfo_cpu =~ /\bF16C\b/
-	else
+  else
     $CFLAGS << " -mfma -mf16c -mavx -mavx2"
-	end
+  end
 end
 
 if uname_m =~ /ppc64.+/
@@ -78,51 +78,51 @@ if uname_m =~ /ppc64.+/
 
   if cpuinfo =~ /\bPOWER9\b/
     $CFLAGS   << " -mcpu=power9"
-		$CXXFLAGS << " -mcpu=power9"
+    $CXXFLAGS << " -mcpu=power9"
   end
 
-	# Require c++23's std::byteswap for big-endian support.
-	CXXFLAGS << " -std=c++23 -DGGML_BIG_ENDIAN" if uname_m == "ppc64"
+  # Require c++23's std::byteswap for big-endian support.
+  CXXFLAGS << " -std=c++23 -DGGML_BIG_ENDIAN" if uname_m == "ppc64"
 end
 
 if ENV.fetch("LLAMA_NO_ACCELERATE", "false") == "false"
   puts "got past first if"
-	# Mac M1 - include Accelerate framework.
-	# `-framework Accelerate` works on Mac Intel as well, with negliable performance boost (as of the predict time).
-	if uname_s == "Darwin"
-		$CFLAGS  << " -DGGML_USE_ACCELERATE"
-		$LDFLAGS << " -framework Accelerate"
-	end
+  # Mac M1 - include Accelerate framework.
+  # `-framework Accelerate` works on Mac Intel as well, with negliable performance boost (as of the predict time).
+  if uname_s == "Darwin"
+    $CFLAGS  << " -DGGML_USE_ACCELERATE"
+    $LDFLAGS << " -framework Accelerate"
+  end
 end
 
 if ENV.fetch("LLAMA_OPENBLAS", "false") == "true"
-	$CFLAGS  << " -DGGML_USE_OPENBLAS -I/usr/local/include/openblas"
-	have_library("openblas")
+  $CFLAGS  << " -DGGML_USE_OPENBLAS -I/usr/local/include/openblas"
+  have_library("openblas")
 end
 
 if ENV.fetch("LLAMA_GPROF", "true") == "true"
-	$CFLAGS   << " -pg"
-	$CXXFLAGS << " -pg"
+  $CFLAGS   << " -pg"
+  $CXXFLAGS << " -pg"
 end
 
 if uname_m =~ /aarch64.+/
-	$CFLAGS   << " -mcpu=native"
-	$CXXFLAGS << " -mcpu=native"
+  $CFLAGS   << " -mcpu=native"
+  $CXXFLAGS << " -mcpu=native"
 end
 
 if uname_m =~ /armv6.+/
-	# Raspberry Pi 1, 2, 3
-	$CFLAGS << " -mfpu=neon-fp-armv8 -mfp16-format=ieee -mno-unaligned-access"
+  # Raspberry Pi 1, 2, 3
+  $CFLAGS << " -mfpu=neon-fp-armv8 -mfp16-format=ieee -mno-unaligned-access"
 end
 
 if uname_m =~ /armv7.+/
-	# Raspberry Pi 4
-	$CFLAGS << " -mfpu=neon-fp-armv8 -mfp16-format=ieee -mno-unaligned-access -funsafe-math-optimizations"
+  # Raspberry Pi 4
+  $CFLAGS << " -mfpu=neon-fp-armv8 -mfp16-format=ieee -mno-unaligned-access -funsafe-math-optimizations"
 end
 
 if uname_m =~ /armv8.+/
-	# Raspberry Pi 4
-	$CFLAGS << " -mfp16-format=ieee -mno-unaligned-access"
+  # Raspberry Pi 4
+  $CFLAGS << " -mfp16-format=ieee -mno-unaligned-access"
 end
 
 include_paths = [
